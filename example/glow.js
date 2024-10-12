8658d88eacd8905c8d7b7b1fcd2db141eab36e67
RandomGamingDev <randomgamingdev@gmail.com>
2024-10-12 17:34:33 -0400

    the actual code stuff

---
+++ example/glow.js
@@ -0,0 +1,179 @@
+class Glow {
+  constructor(rdr = window, canvas = _renderer) {
+    this.rdr = rdr;
+    this.canvas = canvas;
+    if (Glow.shader == null)
+      Glow.shader =
+        this.rdr.createShader(Glow.shader_vert, Glow.shader_frag);
+    this.light_buf = this.rdr.createFramebuffer();
+    this.gradients = [];
+  }
+  
+  push_gradient(gradient) {
+    const img = this.rdr.createImage(gradient[0].length, gradient.length);
+    img.loadPixels();
+    {
+      for (const i in gradient) {
+        const moment_gradient = gradient[i];
+        for (const j in moment_gradient)
+          img.set(j, i, moment_gradient[j]);
+      }
+    }
+    img.updatePixels();
+    return this.gradients.push(img) - 1;
+  }
+  
+  pop_gradient(gradient_id) {
+    this.gradients.pop(gradient_id);
+  }
+  
+  queue_point_light(x, y, gradient, center_uv, edge_uv, radius, preprocess_buf, slice_density, sample_length, internal = false, angle_range = [0.0, 2 * Math.PI], threshold = 1) {
+    this.light_buf.begin();
+    {
+      this.rdr.shader(Glow.shader);
+      Glow.shader.setUniform("gradient", this.gradients[gradient]);
+      Glow.shader.setUniform("centerUV", center_uv);
+      Glow.shader.setUniform("radius", radius);
+      Glow.shader.setUniform("preprocessTex", preprocess_buf);
+      Glow.shader.setUniform("sliceDensity", slice_density);
+      Glow.shader.setUniform("sampleLength", sample_length);
+      Glow.shader.setUniform("screenDims", [this.canvas.width, this.canvas.height]);
+      Glow.shader.setUniform("internal", internal);
+      Glow.shader.setUniform("angleOff", angle_range[0]);
+      Glow.shader.setUniform("threshold", threshold);
+      {
+        const num_slices = floor(slice_density * (angle_range[1] - angle_range[0]) / (2 * Math.PI)) + 1;
+        this.rdr.beginShape(TRIANGLE_FAN);
+        {
+          this.rdr.vertex(x, y, 0, center_uv[0], center_uv[1]);
+          for (let i = 0; i < num_slices; i++)
+            this.rdr.vertex(x, y, 0, edge_uv[0], edge_uv[1]);
+        }
+        this.rdr.endShape(CLOSE);
+      }
+      this.rdr.resetShader();
+    }
+    this.light_buf.end();
+  }
+  
+  filter() {
+    glow.light_buf.begin();
+    this.rdr.filter(...arguments);
+    glow.light_buf.end();
+  }
+  
+  render() {
+    this.rdr.image(this.light_buf, -this.canvas.width / 2, -this.canvas.height / 2, this.canvas.width, this.canvas.height);
+  }
+  
+  flush() {
+    this.light_buf.begin();
+    this.rdr.clear();
+    this.light_buf.end();
+  }
+  
+  render_flush() {
+    this.render();
+    this.flush();
+  }
+
+  debug_view_gradient(gradient_id, uvs, uvs_disp_color = [255, 0, 0, 255], background_color = [0, 0, 0, 255]) {
+    if (background_color == null)
+      this.rdr.clear();
+    else
+      this.rdr.background(background_color);
+
+    this.rdr.image(glow.gradients[gradient_id], -this.canvas.width / 2, -this.canvas.height / 2, this.canvas.width, this.canvas.height);
+
+    push();
+    {
+      stroke(uvs_disp_color);
+      for (const uv of uvs) {
+        const uv00 = (uv[0][0] - 0.5) * this.canvas.width;
+        const uv01 = (uv[0][1] - 0.5) * this.canvas.height;
+        const uv10 = (uv[1][0] - 0.5) * this.canvas.width;
+        const uv11 = (uv[1][1] - 0.5) * this.canvas.height;
+        point(uv00, uv01);
+        point(uv10, uv11);
+        line(uv00, uv01, uv10, uv11);
+      }
+    }
+    pop();
+  }
+}
+
+// volumetric lights & lights of different shapes
+
+Glow.shader_vert =
+`#version 300 es
+
+precision mediump float;
+
+#define PI 3.1415926535897932384626433832795
+
+in vec3 aPosition;
+in vec2 aTexCoord;
+
+out vec2 vTexCoord;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+uniform vec2 centerUV;
+uniform sampler2D preprocessTex;
+uniform float radius;
+uniform float sampleLength;
+uniform int sliceDensity;
+uniform ivec2 screenDims;
+uniform bool internal;
+uniform float angleOff;
+uniform float threshold;
+
+void main() {
+  if (gl_VertexID == 0) {
+    vTexCoord = aTexCoord;
+    gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aPosition, 1.0);
+    return;
+  }
+  
+  float angle = angleOff + 2.0 * PI * float(gl_VertexID) / float(sliceDensity);
+  vec2 vec = radius * vec2(cos(angle), sin(angle));
+  
+  float numSamples = radius / sampleLength;
+  vec2 vpDist = vec / (numSamples * vec2(screenDims));
+
+  vec2 cursor = 0.5 + aPosition.xy / vec2(screenDims);
+  float i = 0.0;
+  if (internal) {
+    for (; i < numSamples; i += 1.0) {
+      if (texture(preprocessTex, cursor).a < threshold)
+        break;
+      cursor += vpDist;
+    }
+  }
+  for (; i < numSamples; i += 1.0) {
+    if (texture(preprocessTex, cursor).a >= threshold)
+      break;
+    cursor += vpDist;
+  }
+
+  vTexCoord = centerUV + (i / numSamples) * (aTexCoord - centerUV);
+  vec4 positionVec4 = vec4(vec2(screenDims) * (cursor - 0.5), 0.0, 1.0);
+  gl_Position = uProjectionMatrix * uModelViewMatrix * positionVec4;
+}`;
+Glow.shader_frag =
+`#version 300 es
+precision mediump float;
+    
+in vec2 vTexCoord;
+
+out vec4 fragColor;    
+
+uniform sampler2D gradient;
+
+void main() {
+  vec4 color = texture(gradient, vTexCoord);
+  fragColor = color * color.a;
+}
+`;
+Glow.shader = null;
---
+++ example/sketch.js
@@ -0,0 +1,107 @@
+let test_img;
+let font;
+function preload() {
+  test_img = loadImage("test.png");
+  font = loadFont("FFFFORWA.TTF");
+}
+
+let canvas;
+let preprocessingBuffer;
+let glow;
+let animated_rainbow_gradient;
+let black_to_white_gradient;
+let rainbow_gradient;
+function setup() {
+  canvas = createCanvas(400, 400, WEBGL);
+  noStroke();
+  noSmooth();
+  setAttributes({ alpha: true });
+  
+  _renderer.getTexture(test_img).setInterpolation(NEAREST, NEAREST);
+  
+  preprocessingBuffer = createFramebuffer();
+  glow = new Glow();
+
+  let gradient = [];
+  let rainbow = [
+    [255, 0, 0, 255], // RED
+    [254, 138, 24, 255], // ORANGE
+    [255, 255, 0, 255], // YELLOW
+    [0, 255, 0, 255], // GREEN
+    [0, 0, 255, 255], // BLUE
+    [75, 0, 130, 255], // INDIGO
+    [127, 0, 255, 255] // VIOLET
+  ];
+  for (const i in rainbow)
+    gradient.push(rainbow.slice(i, rainbow.length).concat(rainbow.slice(0, i)));
+  animated_rainbow_gradient = glow.push_gradient(gradient);
+  black_to_white_gradient = glow.push_gradient([
+    [[255, 255, 255, 255], [255, 255, 255, 0]],
+  ]);
+  rainbow_gradient = glow.push_gradient([
+    [
+      [255, 0, 0, 255], // RED
+      [254, 138, 24, 255], // ORANGE
+      [255, 255, 0, 255], // YELLOW
+      [0, 255, 0, 255], // GREEN
+      [0, 0, 255, 255], // BLUE
+      [75, 0, 130, 255], // INDIGO
+      [127, 0, 255, 255] // VIOLET
+    ],
+  ]);
+}
+
+function draw() {
+  // Draw all the normal canvas things you'd draw on a canvas for preprocessors to use
+  preprocessingBuffer.begin();
+  {
+    clear();
+    textFont(font);
+    text(ceil(frameRate()), -width / 2 + 10, -height / 2 + 25);
+    image(test_img, -100, -100, 200, 200);
+    rect(40, 20, 100, 100);
+  }
+  preprocessingBuffer.end();
+  
+  // Draw all the background stuff you want behind the lights
+  background(50);
+  // Draw the light (choose which one you want)
+  const LightTypes = {
+    NormalWhite: 0,
+    TrippyRainbow: 1
+  };
+  const lightType = LightTypes.NormalWhite;
+  switch (lightType) {
+    case LightTypes.NormalWhite:
+      glow.queue_point_light(
+        x = mouseX - width / 2,
+        y = mouseY - height / 2,
+        gradient = black_to_white_gradient,
+        center_uv = [0.25, 0.5],
+        edge_uv = [0.75, 0.5],
+        radius = 100,
+        preprocess_buf = preprocessingBuffer,
+        slice_density = 2 ** 7,
+        sample_length = 1
+      );
+      break;
+    case LightTypes.TrippyRainbow:
+      // Trippy Rainbow Light
+      const sample_y = 1.0 - (millis() / 1000) % 1;
+      glow.queue_point_light(
+        x = mouseX - width / 2,
+        y = mouseY - height / 2,
+        gradient = animated_rainbow_gradient,
+        center_uv = [0, sample_y],
+        edge_uv = [1, sample_y],
+        radius = 100,
+        preprocess_buf = preprocessingBuffer,
+        slice_density = 2 ** 7,
+        sample_length = 1
+      );
+      break;
+  }
+  glow.filter(BLUR); // Blur everything so that disturbances from the lower resolution of light are less visible and for an antialiasing effect
+  glow.render_flush(); // Render everything and clear the light buffer
+  image(preprocessingBuffer, -width / 2, -height / 2, width, height); // Now draw the preprocessor buffer to the actual screen
+}
